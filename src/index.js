b1b5321065c04c4a7ca7b7e6c0d284c2fc93c907
Livia Nascimento <livseraus@gmail.com>
2019-08-20 17:55:55 -0300

    Adicionando Event Listener no clique do botão

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 // criar as funções do html
 
-function encodeMsg() {
+//document.getElementById("btn_encode").addEventListener("click", encodeMsg);
+const encodeMsg = document.getElementById("btn_encode");
+
+encodeMsg.addEventListener("click", () => {
     let msg = document.getElementById("str").value.toUpperCase();
     let offset = parseInt(document.getElementById("offset").value);
     let encodedMsg = window.cipher.encode(offset, msg);
 
     document.getElementById("encodedMsg").innerHTML = `Mensagem codificada: ${encodedMsg}`;
     document.getElementById("decodedMsg").innerHTML = "";
-}
+
+});
+
+// function encodeMsg() {
+//     let msg = document.getElementById("str").value.toUpperCase();
+//     let offset = parseInt(document.getElementById("offset").value);
+//     let encodedMsg = window.cipher.encode(offset, msg);
+
+//     document.getElementById("encodedMsg").innerHTML = `Mensagem codificada: ${encodedMsg}`;
+//     document.getElementById("decodedMsg").innerHTML = "";
+// }
 
 function decodeMsg() {
     let msg = document.getElementById("str").value.toUpperCase();

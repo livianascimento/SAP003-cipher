1624becb409457ea002e464cf08122d04e20b249
Livia Nascimento <livseraus@gmail.com>
2019-08-22 17:38:01 -0300

    Mudança no código e Hacker Edition

--- src/cipher.js
+++ src/cipher.js
@@ -6,8 +6,19 @@ window.cipher = {
 function encode(offset, str) {
   let msg = "";
 
-  for (let i = 0; i < str.length; i++) {
+  /*for (let i = 0; i < str.length; i++) {
     msg += String.fromCharCode(((str.charCodeAt(i) - 65) + offset) % 26 + 65);
+  }*/
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      msg += String.fromCharCode(((str.charCodeAt(i) - 97) + offset) % 26 + 97);
+    }
+    else if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      msg += String.fromCharCode(((str.charCodeAt(i) - 65) + offset) % 26 + 65);
+    }
+    else {
+      msg += str[i];
+    }
   }
 
   return msg;
@@ -19,6 +30,17 @@ function decode(offset, str) {
   for (let i = 0; i < str.length; i++) {    
     msg += String.fromCharCode(((str.charCodeAt(i) + 65) - offset) % 26 + 65);
   }
+  /*for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      msg += String.fromCharCode(((str.charCodeAt(i) + offset) - 65) % 26 + 65);
+    }
+    else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      msg += String.fromCharCode(((str.charCodeAt(i) + offset) - 95) % 26 + 95);
+    }
+    else {
+      msg += str[i];
+    }
+  }*/
 
   return msg;
 }
--- src/index.js
+++ src/index.js
@@ -2,26 +2,17 @@ const encodeMsg = document.getElementById("btn_encode");
 const decodeMsg = document.getElementById("btn_decode");
 
 encodeMsg.addEventListener("click", () => {
-    const msg = document.getElementById("str").value.toUpperCase();
-    const offset = parseInt(document.getElementById("offset").value);
-    const encodedMsg = window.cipher.encode(offset, msg);
+  const msg = document.getElementById("str").value;
+  const offset = parseInt(document.getElementById("offset").value);
+  const encodedMsg = window.cipher.encode(offset, msg);
 
-    document.getElementById("data_output").innerHTML = `<p>Mensagem Codificada</p><p>${encodedMsg}</p>`;    
+  document.getElementById("data_output").innerHTML = `<p>Mensagem Codificada</p><p>${encodedMsg}</p>`;    
 });
 
 decodeMsg.addEventListener("click", () => {
-    const msg = document.getElementById("str").value.toUpperCase();
-    const offset = parseInt(document.getElementById("offset").value);
-    const decodedMsg = window.cipher.decode(offset, msg);
+  const msg = document.getElementById("str").value.toUpperCase();
+  const offset = parseInt(document.getElementById("offset").value);
+  const decodedMsg = window.cipher.decode(offset, msg);
 
-    document.getElementById("data_output").innerHTML = `<p>Mensagem Decodificada</p><p>${decodedMsg}</p>`;
+  document.getElementById("data_output").innerHTML = `<p>Mensagem Decodificada</p><p>${decodedMsg}</p>`;
 });
-
-
-/*const getDataInput = () => {
-    const arr = [];
-    arr[0] = parseInt(document.getElementById("offset").value);
-    arr[1] = document.getElementById("str").value.toUpperCase();
-    
-    return arr;    
-};*/

caefbf591d1bfa2ea1b676943dc1826a08ee5499
Livia Nascimento <livseraus@gmail.com>
2019-08-23 14:22:31 -0300

    Adicionando o Hack Edition das letras minúsculas e caracteres especiais

--- src/cipher.js
+++ src/cipher.js
@@ -6,9 +6,6 @@ window.cipher = {
 function encode(offset, str) {
   let msg = "";
 
-  /*for (let i = 0; i < str.length; i++) {
-    msg += String.fromCharCode(((str.charCodeAt(i) - 65) + offset) % 26 + 65);
-  }*/
   for (let i = 0; i < str.length; i++) {
     if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
       msg += String.fromCharCode(((str.charCodeAt(i) - 97) + offset) % 26 + 97);
@@ -27,20 +24,17 @@ function encode(offset, str) {
 function decode(offset, str) {
   let msg = "";
 
-  for (let i = 0; i < str.length; i++) {    
-    msg += String.fromCharCode(((str.charCodeAt(i) + 65) - offset) % 26 + 65);
-  }
-  /*for (let i = 0; i < str.length; i++) {
-    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-      msg += String.fromCharCode(((str.charCodeAt(i) + offset) - 65) % 26 + 65);
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      msg += String.fromCharCode(((str.charCodeAt(i) - 122) - offset) % 26 + 122);
     }
-    else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
-      msg += String.fromCharCode(((str.charCodeAt(i) + offset) - 95) % 26 + 95);
+    else if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      msg += String.fromCharCode(((str.charCodeAt(i) - 90) - offset) % 26 + 90);
     }
     else {
       msg += str[i];
     }
-  }*/
+  }
 
   return msg;
 }
